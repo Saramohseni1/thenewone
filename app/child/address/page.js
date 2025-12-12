b2c42e754e6deced1285e803eba045113f861098
Saramohseni1 <130564385+Saramohseni1@users.noreply.github.com>
2025-12-13 01:42:06 +0330

    Update page.js

--- app/child/address/page.js
+++ app/child/address/page.js
@@ -1,4 +1,6 @@
 "use client";
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 
 import { useSearchParams, useRouter } from "next/navigation";
 import { useState } from "react";
